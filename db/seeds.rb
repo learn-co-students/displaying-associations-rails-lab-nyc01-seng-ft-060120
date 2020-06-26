9666e6fc60072dc5bf69f67ed49a8205a5985efe
shadman-a <shadman@outlook.com>
2020-06-25 22:02:44 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+
+Artist.create(name: "mann")
+Song.create(title: "songg", artist_id: Artist.all.sample.id)
+
+  
+
+  
